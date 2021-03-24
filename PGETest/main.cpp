36a66af65adbac951e184da7332f3c170ccdf898
Karl Muller <karlmuller10@gmail.com>
2021-03-23 21:15:07 -0400

    Implemented a Main Menu and espcape to main menu feature

--- PGETest/main.cpp
+++ PGETest/main.cpp
@@ -51,8 +51,13 @@ public:
 	// Rooms variable
 	Rooms r;
 
-	// Menu
+	// Menu variables
+	olc::Sprite* sprMenu = nullptr;
+	olc::Decal* decMenu = nullptr;
 
+	// Main Menu
+	bool MAINMENUACTIVE = true;
+	
 
 
 public:
@@ -83,119 +88,156 @@ public:
 
 		}
 
+		// loads menu sprite
+		sprMenu = new olc::Sprite("C:/Users/Karl/Documents/GameSourceArt/DialogBox.png");
+		decMenu = new olc::Decal(sprMenu);
+
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// Called once per frame, draws random coloured pixels
-		//for (int x = 0; x < ScreenWidth(); x++)
-			//for (int y = 0; y < ScreenHeight(); y++)
-			//	Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
-
-		// Clears screen
-		Clear(olc::VERY_DARK_GREY);
-
-	
-		// draws wall sprites
-		for (int i = 0; i < std::size(r.room1); i++)
+		if (MAINMENUACTIVE) // Main Menu
 		{
-			for (int j = 0; j < std::size(r.room1[i]); j++)
-			{
-				if (r.room1[i][j].type == Tile::TileType::WALL)
-				{
-					// draws wall sprites
-					DrawDecal({ float(j * 100), float(i * 100) }, decWall);
+			Clear(olc::DARK_BLUE);
+			std::string startGame = "Start Game";
 
-					// Collision rectangle for wall tiles
-					DrawRect({ int(j * 100), int(i * 100) }, { int(r.room1[i][j].tileSize), int(r.room1[i][j].tileSize) }, olc::GREEN);
+			DrawString({ 185, 100 }, startGame, olc::WHITE, 3);
+
+			olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY()) };
+
+			if (mouse.x >= 185 && mouse.x <= 425 && mouse.y >= 90 && mouse.y <= 125)
+			{
+				DrawString({ 185, 100 }, startGame, olc::YELLOW, 3);
+				if (GetMouse(0).bPressed)
+					MAINMENUACTIVE = false;
+			}
+
+		}
+		else // Playing Field
+		{
+
+
+
+			// Clears screen
+			Clear(olc::VERY_DARK_GREY);
+
+
+			// draws wall sprites
+			for (int i = 0; i < std::size(r.room1); i++)
+			{
+				for (int j = 0; j < std::size(r.room1[i]); j++)
+				{
+					if (r.room1[i][j].type == Tile::TileType::WALL)
+					{
+						// draws wall sprites
+						DrawDecal({ float(j * 100), float(i * 100) }, decWall);
+
+						// Collision rectangle for wall tiles
+						DrawRect({ int(j * 100), int(i * 100) }, { int(r.room1[i][j].tileSize), int(r.room1[i][j].tileSize) }, olc::GREEN);
+
+					}
 
 				}
-
 			}
-		}
 
-		bool didPlayerCollide = playerCollide(r.room1);
+			bool didPlayerCollide = playerCollide(r.room1);
 
-		// Player collisions with objects which sends the player away from the current key they just pressed
-		if (didPlayerCollide)
-		{
-			if (currentKey == olc::Key::W)
+			// Player collisions with objects which sends the player away from the current key they just pressed
+			if (didPlayerCollide)
 			{
-				yPosition++;
-				allowKeyW = false;
+				if (currentKey == olc::Key::W)
+				{
+					yPosition++;
+					allowKeyW = false;
+				}
+				else if (currentKey == olc::Key::A)
+				{
+					xPosition++;
+					allowKeyA = false;
+				}
+				else if (currentKey == olc::Key::S)
+				{
+					yPosition--;
+					allowKeyS = false;
+				}
+				else if (currentKey == olc::Key::D)
+				{
+					xPosition--;
+					allowKeyD = false;
+				}
 			}
-			else if (currentKey == olc::Key::A)
+
+
+
+			// Drawing player collision box
+			playerCollisionTopLeft = { float(xPosition), float(yPosition) };
+			//DrawRect(playerCollisionTopLeft, playerCollisionBotRight, olc::RED);
+
+			// Position of mouse cursor
+			olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY()) };
+
+			// WASD controls affected by movespeed
+			if (GetKey(olc::Key::D).bHeld && allowKeyD)
+			{
+				xPosition += moveSpeed;
+				currentKey = olc::Key::D;
+			}
+			else if (GetKey(olc::Key::A).bHeld && allowKeyA)
+			{
+				xPosition -= moveSpeed;
+				currentKey = olc::Key::A;
+			}
+			else if (GetKey(olc::Key::W).bHeld && allowKeyW)
+			{
+				yPosition -= moveSpeed;
+				currentKey = olc::Key::W;
+			}
+			else if (GetKey(olc::Key::S).bHeld && allowKeyS)
+			{
+				yPosition += moveSpeed;
+				currentKey = olc::Key::S;
+			}
+
+			// Shift causes player to run
+			if (GetKey(olc::Key::SHIFT).bHeld)
+				moveSpeed = 0.75;
+			else
+				moveSpeed = 0.5;
+
+			//Override collision keys
+			if (GetKey(olc::Key::E).bHeld)
 			{
 				xPosition++;
-				allowKeyA = false;
+				moveSpeed = 0.5;
 			}
-			else if (currentKey == olc::Key::S)
-			{
-				yPosition--;
-				allowKeyS = false;
-			}
-			else if (currentKey == olc::Key::D)
+			if (GetKey(olc::Key::Q).bHeld)
 			{
 				xPosition--;
-				allowKeyD = false;
+				moveSpeed = 0.5;
 			}
+
+			// Keys allowed to press are reset after collision stops them from being allowed
+			allowKeyW = true, allowKeyA = true, allowKeyS = true, allowKeyD = true;
+
+			// Draws player sprite
+			DrawDecal({ float(xPosition), float(yPosition) }, decHam, { PLAYER_SPRITE_SCALE, PLAYER_SPRITE_SCALE });
+
+
+
+
+
+
+
+			// escapes to main menu
+			if (GetKey(olc::Key::ESCAPE).bPressed)
+			{
+				MAINMENUACTIVE = true;
+			}
+
 		}
 		
-
-
-		// Drawing player collision box
-		playerCollisionTopLeft = { float(xPosition), float(yPosition) };
-		//DrawRect(playerCollisionTopLeft, playerCollisionBotRight, olc::RED);
-
-		// Position of mouse cursor
-		olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY()) };
-
-		// WASD controls affected by movespeed
-		if (GetKey(olc::Key::D).bHeld && allowKeyD)
-		{
-			xPosition += moveSpeed;
-			currentKey = olc::Key::D;
-		}
-		else if (GetKey(olc::Key::A).bHeld && allowKeyA)
-		{
-			xPosition -= moveSpeed;
-			currentKey = olc::Key::A;
-		}
-		else if (GetKey(olc::Key::W).bHeld && allowKeyW)
-		{
-			yPosition -= moveSpeed;
-			currentKey = olc::Key::W;
-		}
-		else if (GetKey(olc::Key::S).bHeld && allowKeyS)
-		{
-			yPosition += moveSpeed;
-			currentKey = olc::Key::S;
-		}
-		
-		// Shift causes player to run
-		if (GetKey(olc::Key::SHIFT).bHeld)
-			moveSpeed = 0.75;
-		else
-			moveSpeed = 0.5;
-
-		//Override collision keys
-		if (GetKey(olc::Key::E).bHeld)
-		{
-			xPosition++;
-			moveSpeed = 0.5;
-		}
-		if (GetKey(olc::Key::Q).bHeld)
-		{
-			xPosition--;
-			moveSpeed = 0.5;
-		}
-
-		// Keys allowed to press are reset after collision stops them from being allowed
-		allowKeyW = true, allowKeyA = true, allowKeyS = true, allowKeyD = true;
-		
-		// Draws player sprite
-		DrawDecal({ float(xPosition), float(yPosition) }, decHam, { PLAYER_SPRITE_SCALE, PLAYER_SPRITE_SCALE });
 		
 		return true;
 	}
